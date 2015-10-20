a82666d698259a694323f2066f9daf7adcc73265
eikonomega <mike@eikonomega.com>
2015-10-20 11:25:59 -0400

    Brute Force Solution to Question 7

--- project_euler/question_7.py
+++ project_euler/question_7.py
@@ -5,17 +5,28 @@ What is the 10 001st prime number?
 """
 
 
-def is_prime(possible_prime):
+def is_prime(possible_prime: int) -> bool:
+    """
+    Attempt to divide a given number by all natural numbers between
+    2 and n-1.  If any have a modolo value of 0 then it isn't a prime number.
+
+    Args:
+        possible_prime: An integer to be evaluated as a possible prime number.
+
+    Returns:
+        True if possible_prime is a prime number, otherwise False.
+    """
     for item in range(2, possible_prime):
-        print(possible_prime, item)
         if not possible_prime % item:
             return False
     return True
 
-
-primes = []
-for possibility in range(2, 100):
+possibility = 2
+primes = list()
+while len(primes) < 10001:
     if is_prime(possibility):
         primes.append(possibility)
+    possibility += 1
 
-print(primes)
+print(primes[-1])
+
