7a1b2643dfc5c358ed31687b7307255556904ce0
eikonomega <mike@eikonomega.com>
2015-11-03 18:30:21 -0500

    Brute Force Solution to Problem 8

--- data_structures/container_efficiencies.py
+++ data_structures/container_efficiencies.py
@@ -13,26 +13,78 @@ def iterate(test_object):
     for item in test_object:
         pass
 
+
+def membership_test(test_container):
+    if 25000 in test_container:
+        pass
+
+print("\n## SPACE EFFICIENCY TESTS ##")
 test_tuple = tuple(range(500000))
 test_list = list(range(500000))
 test_set = set(range(500000))
-test_array = array()
+test_array = array.array('i', range(500000))
 
-print(sys.getsizeof(test_tuple))
-print(sys.getsizeof(test_list))
-print(sys.getsizeof(test_set))
+print("Tuple:", sys.getsizeof(test_tuple))
+print("List:", sys.getsizeof(test_list))
+print("Set:", sys.getsizeof(test_set))
+print("Array(int):", sys.getsizeof(test_array))
 
-print(
-    timeit.timeit('iterate(test_tuple)',
-                  setup="from __main__ import iterate, test_tuple, test_list",
-                  number=1000))
+print("\n## ITERATION SPEED TESTS ##")
 
 print(
-    timeit.timeit('iterate(test_list)',
-                  setup="from __main__ import iterate, test_tuple, test_list",
-                  number=1000))
+    "Tuple Iteration:",
+    timeit.timeit(
+        'iterate(test_tuple)',
+        setup="from __main__ import iterate, test_tuple",
+        number=200))
 
 print(
-    timeit.timeit('iterate(test_set)',
-                  setup="from __main__ import iterate, test_tuple, test_list, test_set",
-                  number=1000))
+    "List Iteration:",
+    timeit.timeit(
+        'iterate(test_list)',
+        setup="from __main__ import iterate, test_list",
+        number=200))
+
+print(
+    "Set Iteration:",
+    timeit.timeit(
+        'iterate(test_set)',
+        setup="from __main__ import iterate, test_set",
+        number=200))
+
+print(
+    "Array(int) Iteration:",
+    timeit.timeit(
+        'iterate(test_array)',
+        setup="from __main__ import iterate, test_array",
+        number=200))
+
+
+print("\n## MEMBERSHIP SPEED TESTS ##")
+print(
+    "Tuple Iteration:",
+    timeit.timeit(
+        'membership_test(test_tuple)',
+        setup="from __main__ import membership_test, test_tuple",
+        number=2000))
+
+print(
+    "List Iteration:",
+    timeit.timeit(
+        'membership_test(test_list)',
+        setup="from __main__ import membership_test, test_list",
+        number=2000))
+
+print(
+    "Set Iteration:",
+    timeit.timeit(
+        'membership_test(test_set)',
+        setup="from __main__ import membership_test, test_set",
+        number=2000))
+
+print(
+    "Array(int) Iteration:",
+    timeit.timeit(
+        'membership_test(test_array)',
+        setup="from __main__ import membership_test, test_array",
+        number=2000))
