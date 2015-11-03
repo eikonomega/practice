3d2de854d4d48ace937cccf15f6121b7c42521fb
eikonomega <mike@eikonomega.com>
2015-11-02 21:55:35 -0500

    Brute Force Solution to Question 7

--- project_euler/question_7.py
+++ project_euler/question_7.py
@@ -1,10 +1,12 @@
 """
-By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
+see that the 6th prime is 13.
 
-What is the 10 001st prime number?
+What is the 10,001st prime number?
 """
 
 
+# Brute Force - n^2 I think
 def is_prime(possible_prime: int) -> bool:
     """
     Attempt to divide a given number by all natural numbers between
